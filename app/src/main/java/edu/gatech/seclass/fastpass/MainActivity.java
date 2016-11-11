4f90eb07cb60323696a041030c86142a9ac1f604
kdunn32 <kdunn32@gatech.edu>
2016-11-11 10:55:56 -0600

    Code to nfc transfer a message added

--- app/src/main/java/edu/gatech/seclass/fastpass/MainActivity.java
+++ app/src/main/java/edu/gatech/seclass/fastpass/MainActivity.java
@@ -1,5 +1,10 @@
 package edu.gatech.seclass.fastpass;
 
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +24,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -27,8 +33,10 @@ import java.util.ArrayList;
 
 import edu.gatech.seclass.fastpass.form;
 
+import static android.nfc.NdefRecord.createMime;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback {
 
     private Button addFormButton;
     private Button recieveFileButton;
@@ -36,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<form> formListAdapter;
     private ListView formListView;
     private DatabaseHelper DBHelper;
+    NfcAdapter mNfcAdapter;
+    TextView textView;
 
 
     @Override
@@ -52,7 +62,37 @@ public class MainActivity extends AppCompatActivity {
         formListAdapter = new ArrayAdapter<form>(MainActivity.this, android.R.layout.simple_list_item_1, listOfForms);
         formListView.setAdapter(formListAdapter);
 
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        // Register callback
+        mNfcAdapter.setNdefPushMessageCallback(this, this);
+
     }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        String text = ("Beam me up, Android!\n\n" +
+                "Beam Time: " + System.currentTimeMillis());
+        NdefMessage msg = new NdefMessage(
+                new NdefRecord[] { createMime(
+                        "application/vnd.com.example.android.beam", text.getBytes())
+                        /**
+                         * The Android Application Record (AAR) is commented out. When a device
+                         * receives a push with an AAR in it, the application specified in the AAR
+                         * is guaranteed to run. The AAR overrides the tag dispatch system.
+                         * You can add it back in to guarantee that this
+                         * activity starts when receiving a beamed message. For now, this code
+                         * uses the tag dispatch system.
+                        */
+                        //,NdefRecord.createApplicationRecord("com.example.android.beam")
+                });
+        return msg;
+    }
+
 
     private void setupViews() {
         addFormButton = (Button) findViewById(R.id.addFormButton);
@@ -246,7 +286,33 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
 
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    void processIntent(Intent intent) {
+        textView = (TextView) findViewById(R.id.textView);
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        textView.setText(new String(msg.getRecords()[0].getPayload()));
+    }
 
 
 }
