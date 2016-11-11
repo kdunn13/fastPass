e2be8e322ab46a704452fe165495727ad6537d13
kdunn32 <kdunn32@gatech.edu>
2016-11-11 14:09:42 -0600

    Added receiver code

--- app/src/main/java/edu/gatech/seclass/fastpass/receiveFormActivity.java
+++ app/src/main/java/edu/gatech/seclass/fastpass/receiveFormActivity.java
@@ -6,7 +6,12 @@ import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONObject;
 
 /**
  * Created by Kevin on 11/11/2016.
@@ -14,11 +19,25 @@ import android.widget.Toast;
 
 public class receiveFormActivity extends AppCompatActivity {
     NfcAdapter mNfcAdapter;
+    private DatabaseHelper DBHelper;
+    Button saveFormButton;
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.receive_form);
+        saveFormButton = (Button) findViewById(R.id.saveForm);
 
+
+        DBHelper = new DatabaseHelper(getApplicationContext());
+
+        saveFormButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveForm();
+            }
+        });
 
 
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
@@ -34,6 +53,17 @@ public class receiveFormActivity extends AppCompatActivity {
         setIntent(intent);
     }
 
+    public void saveForm() {
+        form myForm = new form();
+
+        final EditText firstName = (EditText) findViewById(R.id.firstNameInput);
+        final EditText lastName = (EditText) findViewById(R.id.lastNameInput);
+
+        myForm.firstName = (firstName).getText().toString();
+        myForm.lastName = (lastName).getText().toString();
+
+        DBHelper.insertForm(myForm);
+    }
 
     @Override
     public void onResume() {
@@ -45,13 +75,34 @@ public class receiveFormActivity extends AppCompatActivity {
     }
 
         void processIntent(Intent intent) {
-            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
-            NfcAdapter.EXTRA_NDEF_MESSAGES);
-        // only one message sent during the beam
-            NdefMessage msg = (NdefMessage) rawMsgs[0];
-        // record 0 contains the MIME type, record 1 is the AAR, if present
+            try {
+                String formJsonString;
+                JSONObject formJson;
+                form myForm = new form();
+                final EditText firstName = (EditText) findViewById(R.id.firstNameInput);
+                final EditText lastName = (EditText) findViewById(R.id.lastNameInput);
 
-            Toast.makeText(this, new String(msg.getRecords()[0].getPayload()), Toast.LENGTH_LONG).show();
+
+                Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                        NfcAdapter.EXTRA_NDEF_MESSAGES);
+                // only one message sent during the beam
+                NdefMessage msg = (NdefMessage) rawMsgs[0];
+
+                // record 0 contains the MIME type, record 1 is the AAR, if present
+                formJsonString = new String(msg.getRecords()[0].getPayload());
+                formJson = new JSONObject(formJsonString);
+
+                myForm.firstName = formJson.getString("firstName");
+                myForm.lastName = formJson.getString("lastName");
+
+                firstName.setText(myForm.firstName);
+                lastName.setText(myForm.lastName);
+
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
 
     }
 }
